f5718fdcc1795cbecb272a223b43e0c9ed7e5511
Sayaka <alice@lolita.technology>
2024-03-09 12:56:03 +0100

    fix missing includes on base_convert

--- sayaslicer/base_convert.hpp
+++ sayaslicer/base_convert.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <cmath>
 
 const std::string VerifyBaseAndConvert(const char* buf, int base);
 int ToBaseString(char* buf, int buf_size, const void* p_data, int base);
